f76777402adadf4ee90739675a4ae0ed6e6bd104
Ahamed Noyon <80954363+ahamednoyon@users.noreply.github.com>
2021-06-10 11:53:42 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Bxslider slider
+
+$(document).ready(function(){
+    $('.slider_Area').bxSlider();
+    $('#main_menu').meanmenu({
+      meanMenuContainer:'#mobile_menu',
+      meanScreenWidth: 991,
+    });
+  });
